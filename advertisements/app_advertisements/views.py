bd43e4fd47d1028b69d2cbdce7bfb112909719be
TanPetr <136500782+TanPetr@users.noreply.github.com>
2023-08-30 11:53:35 +0300

    Add files via upload
    готовый проект

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -2,14 +2,36 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import Advertisement
 from .forms import AdvertisementForm
+from django.contrib.auth import get_user_model
+from django.db.models import Count
+
+User = get_user_model()
 
 def index(requests):
-    advertisements = Advertisement.objects.all()
-    context = {'advertisements' : advertisements}
+    title = requests.GET.get('query')
+    if title:
+        advertisements = Advertisement.objects.filter(title__icontains = title)
+    else:
+        advertisements = Advertisement.objects.all()
+    context = {'advertisements' : advertisements,
+               'title' : title,
+               }
     return render(requests, 'app_advertisements/index.html', context)
 
+def advertisement_detail(request, pk):
+    advertisement = Advertisement.objects.get(id = pk)
+    context = {
+        'advertisement' : advertisement
+    }
+    return render(request, 'app_advertisements/advertisement.html', context)
+
 def top_sellers(requests):
-    return render (requests, 'app_advertisements/top-sellers.html')
+    users = User.objects.annotate(
+        adv_count = Count('advertisment')
+    ).order_by('-adv_count')
+
+    context = { 'users' : users}
+    return render (requests, 'app_advertisements/top-sellers.html', context)
 
 
 
